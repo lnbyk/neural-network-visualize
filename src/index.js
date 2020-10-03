dbb525a5f01eee39477d91a30ac8e43f839148d1
边如柏 <lnbyk@udel.edu>
2020-10-02 21:47:54 -0400

    update layout and add bias

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Routes from './routes';
 import './style.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 class App extends Component {
   constructor() {
     super();
